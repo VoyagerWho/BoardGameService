57234867957c05a0612973d8d11b8e0479c3926d
VoyagerWho <blind002525@gmail.com>
2022-08-03 21:18:41 +0200

    Hotfix 8

--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -422,7 +422,6 @@ router.get('/:id/Join', (req, res) => {
 	if (rooms.get(rid)) {
 		if (req.session.rooms === undefined) req.session.rooms = {};
 		req.session.rooms[rid] = { uid: -1 };
-		customLog(['Join', req.session.rooms]);
 		req.session.save();
 		res.redirect('/rooms/' + rid);
 	} else res.redirect('/rooms');
